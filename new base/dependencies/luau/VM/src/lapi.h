47c285e7fe457772958bd63261d023fe4e10d9c0
sc <cachebea@gmail.com>
2026-07-09 16:17:14 +0300

    Add files via upload

---
+++ new base/dependencies/luau/VM/src/lapi.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "lobject.h"
+
+LUAI_FUNC const TValue* luaA_toobject(lua_State* L, int idx);
+LUAI_FUNC TValue* index2addr(lua_State* L, int idx);
+LUAI_FUNC const TValue* luaA_toobject(lua_State* L, int idx);
+LUAI_FUNC void luaA_pushobject(lua_State* L, const TValue* o);
+LUAI_FUNC void luaA_pushvalue(lua_State* L, const TValue* o);
+LUAI_FUNC void luaA_pushclass(lua_State* L, LuauClass* lclass);
